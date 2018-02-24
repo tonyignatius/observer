0bb5712e3c3f1970af7c97be81c7f924ae4f7183
Tony A <tignatiu@in.ibm.com>
2018-02-24 20:25:03 +0530

    Comit after geocd call

--- observer/Controllers/ValuesController.cs
+++ observer/Controllers/ValuesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net.Http;
+using System.Text;
 
 namespace observer.Controllers
 {
@@ -15,6 +18,34 @@ namespace observer.Controllers
         {
             return new string[] { "value1", "value2" };
         }
+        // GET api/mail
+        [HttpGet("mail")]
+        public string Mail()
+        {
+            try
+            {
+                //SmtpClient client = new SmtpClient("smtp-mail.outlook.com");
+                SmtpClient client = new SmtpClient("smtpapps.tus.ams1907.com");
+                //SmtpClient client = new SmtpClient("mail.post.000webhost.com");
+                client.UseDefaultCredentials = true;
+                client.EnableSsl = true;
+                client.Port = 587;
+                //client.Credentials = new NetworkCredential("tonyignatius@live.com", "tripplehorn#1");
+                MailMessage mailmessage = new MailMessage();
+                mailmessage.From = new MailAddress("donotreply@ups.com");
+                mailmessage.To.Add("cyberhero.tony@gmail.com");
+                mailmessage.Body =  "Body of the Message";
+                mailmessage.Subject = "Sent from API";
+                client.Send(mailmessage);
+                return "SentMail";                
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+           
+        }
 
         // GET api/values/5
         [HttpGet("{id}")]
@@ -23,11 +54,110 @@ namespace observer.Controllers
             return "value";
         }
 
+        [HttpGet("geocode/{lat}/{longi}")] //usage http://localhost:52542/api/values/geocode/40.730610/-73.935242
+        public string Geocode(string lat, string longi)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+
+                    string req = locatorRequestJson(lat, longi);
+                    HttpContent content = new StringContent(req, Encoding.UTF8, "application/json");
+                    client.BaseAddress = new Uri("https://onlinetools.ups.com/rest/Locator");
+                    var response = client.PostAsync("https://onlinetools.ups.com/rest/Locator", content).Result;
+                    string gcol = "[";
+                    if (response.IsSuccessStatusCode)
+                    {
+
+                        var jsonresp = GetResponse(response).Result;
+                        Newtonsoft.Json.Linq.JObject json = Newtonsoft.Json.Linq.JObject.Parse(jsonresp);
+                        Console.Write("Success");
+                        foreach (var result in json["LocatorResponse"]["SearchResults"]["DropLocation"])
+                        {
+                            gcol = gcol + "{\"Latitude\":\"" + result["Geocode"]["Latitude"].ToString();
+                            gcol = gcol + "\",\"Longitude\":\"" + result["Geocode"]["Longitude"].ToString() + "\"},";
+                        }
+                        return gcol.Substring(0, gcol.Length - 1) + "]";
+                    }
+                    else
+                        Console.Write("Error");
+
+                    return GetResponse(response).Result;
+                }
+                catch(Exception ex) { return ex.Message; }
+                }            
+        }
+
+        
+
+        async Task<string> GetResponse(HttpResponseMessage rspm)
+        {
+            string response = await rspm.Content.ReadAsStringAsync();
+            return response;
+        }
         // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
+
+        public string locatorRequestJson(string latitude, string longitude)
+        {
+            string req = @"{
+  'AccessRequest': {
+    'AccessLicenseNumber': 'ED3E55FEC0B4BE8C',
+    'UserId': 'searchqv',
+    'Password': 'password'
+  },
+  'LocatorRequest': {
+    'Request': {
+      'RequestOption': [
+        '1'
+      ],
+      'TransactionReference': {
+        'CustomerContext': 'Android_Locator'
+      },
+      'RequestAction': 'Locator'
+    },
+    'OriginAddress': {
+      'Geocode': {
+        'Latitude': '" + latitude + @"',
+        'Longitude': '" + longitude + @"'
+      },
+      'AddressKeyFormat': {}
+    },
+    'Translate': {
+      'LanguageCode': 'ENG',
+      'Locale': 'en_US'
+    },
+    'UnitOfMeasurement': {
+      'Code': 'MI'
+    },
+    'LocationSearchCriteria': {
+      'SearchOption': [
+        {
+          'OptionType': {
+            'Code': '01'
+          },
+          'OptionCode': [
+            {
+              'Code': '003'
+            }
+          ]
+        }
+      ],
+      'MaximumListSize': '10',
+      'SearchRadius': '25'
+    },
+    'SortCriteria': {
+      'SortType': '01'
+    }
+  }
+}";
+            return req;
+        }
+
 
         // PUT api/values/5
         [HttpPut("{id}")]
@@ -40,5 +170,6 @@ namespace observer.Controllers
         public void Delete(int id)
         {
         }
+       
     }
 }
---
+++ observer/DataModels/dmodels.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//Using SQLite.Net.Attributes;
+
+namespace observer
+{
+    public class userauth
+    {
+        //[NotNull]
+        //[PrimaryKey]
+        public string UserID
+        { get; set; }
+        //[NotNull]
+        public string password
+        { get; set; }
+        public string profilePath
+        { get; set; }
+    }
+
+    public class profile {
+        //[NotNull]
+        //[PrimaryKey]
+        public string UserID
+        { get; set; }
+        //[NotNull]
+        public string language
+        { get; set; }
+        public string agreement
+        { get; set; }
+        public string profilepic
+        { get; set; }
+        public string fname
+        { get; set; }
+        public string lname
+        { get; set; }
+        public string badge
+        { get; set; }
+        public string updt
+        { get; set; }
+        public string addt
+        { get; set; }        
+    }
+    public class observations {
+        //[NotNull]
+        //[PrimaryKey]
+        public string obsId
+        { get; set; }
+        //[NotNull]
+        public string userid
+        { get; set; }
+        public string assettype
+        { get; set; }
+        public string assetsubtype
+        { get; set; }
+        public string title
+        { get; set; }
+        public string summary
+        { get; set; }
+        public string location
+        { get; set; }
+        //[NotNull]
+        public string status
+        { get; set; }
+        //[NotNull]
+        public string updt
+        { get; set; }
+        public string addt
+        { get; set; }
+    }
+    public class images {
+        //[NotNull]
+        //[PrimaryKey]
+        public string imgId { get; set; }
+        //[NotNull]
+        public string obsid { get; set; }
+        //[NotNull]
+        public string image { get; set; }
+
+    }
+}
